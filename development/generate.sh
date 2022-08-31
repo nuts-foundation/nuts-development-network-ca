cd36755efd842021cdd137d33b0571550c240d75
Rein Krul <info@reinkrul.nl>
2022-08-31 06:26:50 +0200

    Support Windows

--- development/generate.sh
+++ development/generate.sh
@@ -1,3 +1,8 @@
-echo Generating root CA
+if [[ $OSTYPE == msys ]]; then
+  echo cSript does not work on GitBash/Cygwin!
+  exit 1
+fi
+
+echo Generating development network root CA
 openssl ecparam -genkey -name prime256v1 -noout -out ca.key
 openssl req -x509 -new -nodes -key ca.key -sha256 -days 1825 -out ca.pem -subj "/CN=Nuts Development Network Root CA"

7c3b111d26eb55dc09483558bb006cdb38d8766e
Rein Krul <info@reinkrul.nl>
2022-11-03 13:46:34 +0100

    Generate v3 CA certificate instead of v1

--- development/generate.sh
+++ development/generate.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+
 if [[ $OSTYPE == msys ]]; then
   echo Script does not work on GitBash/Cygwin!
   exit 1
 fi
 
+CONFIG="
+[req]
+distinguished_name=dn
+[ dn ]
+[ ext ]
+basicConstraints=CA:TRUE,pathlen:0
+"
+
 echo Generating development network root CA
 openssl ecparam -genkey -name prime256v1 -noout -out ca.key
-openssl req -x509 -new -nodes -key ca.key -sha256 -days 1825 -out ca.pem -subj "/CN=Nuts Development Network Root CA"
+openssl req -config <(echo "$CONFIG") -extensions ext -x509 -new -nodes -key ca.key -sha256 -days 1825 -out ca.pem -subj "/CN=Nuts Development Network Root CA"
